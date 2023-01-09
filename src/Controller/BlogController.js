08b213f0802604bf04d7edbfe4105cabf70106a3
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2023-01-09 10:18:38 +0600

    add blog api done

--- src/Controller/BlogController.js
+++ src/Controller/BlogController.js
@@ -1,21 +1,28 @@
+const Blog = require("../Models/Blog");
+
+//Add Blog
+module.exports.addBlog = (req, res) => {
+    
+    const { title, body, image, author, description, userId, email } = req.body;
+
+    Blog.findOne({ title: title })
+        .exec((error, data) => {
+            //  if(data){return next(new ErrorHander("Tour Package All Ready Register", 500));}
+            const _blog = new Blog({
+                title: title,
+                author: author,
+                body: body,
+                image: image,
+                description: description,
+                slug: title,
+                email: email,
+                userId: userId,
+            });
+            _blog.save((error, data) => {
+                if (error) { return res.status(505).json({ success: "false", error }); }
+                if (data) { return res.status(201).json({ msg: 'Blog Create Successfully', data }) }
+            });
+        });
+}
 
 
-
-//Add Tour Package
-module.exports.addBlog=(req,res,next)=>{
-    const{ title,role,description,short_description,package_startDate,package_endDate,place,price,image,room_type,hotel_name}=req.body; 
-
-    TourPackage.findOne({title:title})
-    .exec((error,data)=>{
-    //  if(data){return next(new ErrorHander("Tour Package All Ready Register", 500));}
-     const _tourPackage=new TourPackage({
-        title,role,image,description,
-        short_description,package_startDate,
-        package_endDate,place,price,image,room_type,hotel_name
-    });
-     _tourPackage.save((error,data)=>{
-      if(error){return res.status(505).json({success:"false",error});}
-      if(data){return res.status(201).json({msg:'Tour Package Create Successfully',data })}
-     });
-    });
-  }
