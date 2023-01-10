729e2922edd9e1e780f0407d4d1a46c73b6d3a66
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2023-01-10 14:56:14 +0600

    related Packages add API

--- src/Controller/TourPackageController.js
+++ src/Controller/TourPackageController.js
@@ -51,7 +51,24 @@ module.exports.getTourPackageDetails=(req,res)=>{
         return res.status(200).json({data,success:"true"})
       }
     })
-  }
+}
+
+// related Packages add
+module.exports.adRelatedPackages = async (req, res) => {
+  const { postId, comment, name } = req.body.commentData;
+  TourPackage.findOneAndUpdate({ _id: postId }, { $push: { comments: req.body.commentData } },
+
+      function (error, success) {
+          if ({ error }) {
+              res.status(500).json({ success: "false", error });
+              console.log(error);
+          } else {
+              res.status(200).json({ msg: 'Your comment has been published', success: "true",data });
+              console.log({ msg: 'Your comment has been published', success: "true" });
+          }
+      });
+}
+
   
 
 
