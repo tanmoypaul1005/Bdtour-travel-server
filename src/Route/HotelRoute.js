bde91deb60a2ac1ad2169d337a038a0b2faee0d4
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2023-01-10 14:46:02 +0600

    delete hotel done

--- src/Route/HotelRoute.js
+++ src/Route/HotelRoute.js
@@ -1,9 +1,10 @@
 const app = require("express");
-const { addHotel, getHotel } = require("../Controller/HotelController");
+const { addHotel, getHotel, deleteHotel } = require("../Controller/HotelController");
 const router = app.Router();
 
 router.post("hotel/add",addHotel),
 router.post("hotel/get",getHotel),
+router.post("hotel/delete",deleteHotel),
 
 
 module.exports=router;
