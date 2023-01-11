6dcac1e1d06015a276a00722f27241351e2d86bf
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2023-01-11 12:56:46 +0600

    addReviewTourPackage

--- src/Route/TourPackageRoute.js
+++ src/Route/TourPackageRoute.js
@@ -1,5 +1,5 @@
 const app = require("express");
-const { addTourPackage, getTourPackage, deleteTourPackage, getTourPackageDetails, addRelatedPackages} = require("../Controller/TourPackageController");
+const { addTourPackage, getTourPackage, deleteTourPackage, getTourPackageDetails, addRelatedPackages, addReviewTourPackage} = require("../Controller/TourPackageController");
 const router = app.Router();
 
 //Add Add Tour Package
@@ -8,4 +8,5 @@ router.get('/tour-package/get',getTourPackage);
 router.post('/tour-package/delete',deleteTourPackage);
 router.get('/tour-package/details/:tourPackageId',getTourPackageDetails);
 router.post('/tour-package/related-package/add',addRelatedPackages);
+router.post('/tour-package/review/add',addReviewTourPackage);
 module.exports = router;
